785cdc42fa3ce241c9104b339ec75e75a9bd43bf
Meghana-Merla <24je0654@iitism.ac.in>
2026-05-17 23:26:23 +0530

    Add replay game functionality

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from game import start_game
 
-start_game()
+
+while True:
+
+    start_game()
+
+    while True:
+
+        choice = input("\nDo you want to play again? (Y/N): ").lower()
+
+        if choice == "y":
+            break
+
+        elif choice == "n":
+            print("\nThanks for playing Hangman!")
+            exit()
+
+        else:
+            print("Please enter Y or N.")
